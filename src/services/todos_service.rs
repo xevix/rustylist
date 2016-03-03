a5eef00ead6bd7af257c81cc1921f25da348e9ad
Alejandro Wainzinger <aikawarazuni@gmail.com>
2016-03-04 01:27:37 +0900

    Better string building.

--- src/services/todos_service.rs
+++ src/services/todos_service.rs
@@ -13,11 +13,17 @@ pub fn fetch_todos() -> String {
     .expect("Error loading todos");
 
     // TODO: find more idiomatic way of concatenating strings
-    let mut s = "".to_string();
+    //    let mut s = "".to_string();
+    //
+    //    for todo in results {
+    //        s.push_str(&(todo.title + "\n" + &todo.body + "\n"));
+    //    }
+    //
+    //    s
 
-    for todo in results {
-        s.push_str(&(todo.title + "\n" + &todo.body + "\n"));
-    }
+    // NOTE: more FP-style, but might be costly
+    // results.iter().map(|todo| "".to_string() + &todo.title + "\n" + &todo.body + "\n").collect::<Vec<String>>().join("\n")
 
-    s
+    // Most efficient way so far, somewhat FP
+    results.iter().fold(String::new(), |out, todo| out + &todo.title + "\n" + &todo.body + "\n")
 }
